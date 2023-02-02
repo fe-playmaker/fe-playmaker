8901e2b1d314b1009c35eae5a7ca0ced939b8307
Mateusz <michalikkontakt@gmail.com>
2023-02-02 17:09:53 +0100

    update

---
+++ client/.next/server/font-loader-manifest.js
@@ -0,0 +1,8 @@
+self.__FONT_LOADER_MANIFEST={
+  "pages": {},
+  "app": {
+    "D:\\Dokumenty\\Files\\projects\\playmaker\\client\\node_modules\\@next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Poppins\",\"arguments\":[{\"weight\":\"400\",\"subsets\":[\"latin\"]}],\"variableName\":\"poppins\"}": [
+      "static/media/916d3686010a8de2.p.woff2"
+    ]
+  }
+}
