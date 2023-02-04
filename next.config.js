4f308930e30906dd3cc65f7f9c2714afe521ed2e
Mateusz <michalikkontakt@gmail.com>
2023-02-04 17:51:31 +0100

    PM-729 Added packages for converting svg to react component

--- next.config.js
+++ next.config.js
@@ -13,6 +13,18 @@ const nextConfig = {
       },
     ],
   },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    }),
+      (config.experiments = {
+        ...config.experiments,
+        ...{ topLevelAwait: true },
+      })
+    return config
+  },
 }
 
 module.exports = nextConfig
