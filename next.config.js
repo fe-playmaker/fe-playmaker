718db14b41b0fba30094f2568bf5535e38fcc1a5
Mateusz <michalikkontakt@gmail.com>
2023-02-04 15:26:45 +0100

    PM-729 Updated configs

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ipla.pluscdn.pl',
+        port: '',
+        pathname: '/dituel/cp/15/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
