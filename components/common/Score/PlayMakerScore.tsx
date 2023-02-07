1cc165d25d4175d82330e4a65c4ca4e87940aac1
Mateusz <michalikkontakt@gmail.com>
2023-02-07 01:14:22 +0100

    updated avatar component

---
+++ components/common/Score/PlayMakerScore.tsx
@@ -0,0 +1,42 @@
+import { cva, VariantProps } from 'class-variance-authority'
+
+import TrendUpIcon from 'img/icons/Trendup.svg'
+
+const pmScoreCva = cva(['text-white', 'p-2'], {
+  variants: {
+    intent: {
+      trendUp: ['bg-greenShade-10'],
+      trendDown: ['bg-orangeShade-10'],
+    },
+    size: {
+      small: ['text-label-md'],
+      big: ['text-label-xl'],
+    },
+  },
+  defaultVariants: {
+    intent: 'trendUp',
+    size: 'small',
+  },
+})
+
+interface Props extends VariantProps<typeof pmScoreCva> {
+  readonly scoreValue: number
+}
+
+export const PlayMakerScore = ({ intent, scoreValue }: Props) => (
+  <div className='flex flex-col items-center gap-2'>
+    <div className='flex items-start'>
+      <div className='flex h-8 w-8 items-center justify-center bg-primaryShade-10' >
+        <span className='font-bold text-primary'>{scoreValue}</span>
+      </div>
+      <div className={pmScoreCva({ intent })}>
+        <p>
+          <TrendUpIcon />
+        </p>
+      </div>
+    </div>
+    <p className='text-center text-caption-sm'>
+      Playmaker <br /> score
+    </p>
+  </div>
+)
