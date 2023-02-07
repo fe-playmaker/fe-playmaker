7cdddb98d571843c7142a57079382154def33df8
dejwi <misti.krasula@gmail.com>
2023-02-07 16:55:29 +0100

    add Navbar

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
 
 import Tabs from '@/components/common/Tabs/tabs'
+import Navbar from '@/components/Navbar/navbar'
 
 const tabs = ['Przegląd', 'Kariera', 'Mecze', 'PlayMaker Score']
 
 const PlayerTestPage = () => (
-  <main>
-    <Tabs tabs={tabs} size="medium">
-      {tabs.map(tab => (
-        <p key={tab}>{tab}</p>
-      ))}
-    </Tabs>
-  </main>
+  <>
+    <Navbar />
+    <main>
+      <Tabs tabs={tabs} size="medium">
+        {tabs.map(tab => (
+          <p key={tab}>{tab}</p>
+        ))}
+      </Tabs>
+    </main>
+  </>
 )
 
 export default PlayerTestPage
