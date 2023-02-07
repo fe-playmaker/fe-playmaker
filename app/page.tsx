4610bf4e966e2a0284538d2bdf72ed54e5e8ef6c
Mateusz <michalikkontakt@gmail.com>
2023-02-07 01:13:23 +0100

    Switched route and added single components to test

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-import { Button } from '../components/common/Button/Button'
-import { Avatar } from '../components/common/user/Avatar'
-import { PlayMakerScore } from '../components/common/user/Score/PlayMakerScore'
-
 const Home = () => (
   <div>
-    <Button intent="primary">Primary</Button>
-    <Avatar />
-    <PlayMakerScore scoreValue={49} />
+    <h1>Homepage</h1>
   </div>
 )
 
--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
 import Tabs from '@/components/common/Tabs/tabs'
+import { Button } from '@/components/common/Button/Button'
+import { Avatar } from '@/components/common/Avatar'
+import { Chips } from '@/components/common/Chips/Chips'
+import { PlayMakerScore } from '@/components/common/Score/PlayMakerScore'
+
+import { USER_DATA } from 'constants/userData'
 
 const tabs = ['Przegląd', 'Kariera', 'Mecze', 'PlayMaker Score']
+const chips = [
+  {
+    title: 'Tylko z udziałem zawodnika',
+    isSelected: true
+  }, 
+  {
+    title: 'Wszystkie', 
+    isSelected: false
+  }, 
+  {
+    title: 'Tylko wideo',
+    isSelected: false
+  }, 
+]
 
 const PlayerTestPage = () => (
   <main>
@@ -11,6 +31,10 @@ const PlayerTestPage = () => (
         <p key={tab}>{tab}</p>
       ))}
     </Tabs>
+    <Button intent="primary">Primary</Button>
+    <Avatar size='small' image={USER_DATA.image} playerName={`${USER_DATA.name} ${USER_DATA.surname}`} />
+    <PlayMakerScore scoreValue={USER_DATA.score} />
+    <Chips chips={chips} />
   </main>
 )
 
