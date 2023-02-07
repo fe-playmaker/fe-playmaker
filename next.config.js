da2e4ac71d303a99692328f67d529f06d9b8a764
dejwi <misti.krasula@gmail.com>
2023-02-07 16:54:25 +0100

    add svg supoort

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    }),
+      (config.experiments = {
+        ...config.experiments,
+        ...{ topLevelAwait: true },
+      })
+    return config
+  },
 }
 
 module.exports = nextConfig
