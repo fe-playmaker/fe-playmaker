be1edcb7370d9bcfd76ad7c7e60bf30a7d5d8a5c
Mateusz <michalikkontakt@gmail.com>
2023-02-08 23:01:23 +0100

    added more components to tests

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,27 +1,26 @@
+import { USER_DATA } from 'constants/userData'
 import React from 'react'
 
-import Tabs from '@/components/common/Tabs/tabs'
+import { Avatar } from '@/components/common/Avatar/Avatar'
 import { Button } from '@/components/common/Button/Button'
-import { Avatar } from '@/components/common/Avatar'
 import { Chips } from '@/components/common/Chips/Chips'
 import { PlayMakerScore } from '@/components/common/Score/PlayMakerScore'
-
-import { USER_DATA } from 'constants/userData'
+import Tabs from '@/components/common/Tabs/tabs'
 
 const tabs = ['Przegląd', 'Kariera', 'Mecze', 'PlayMaker Score']
 const chips = [
   {
     title: 'Tylko z udziałem zawodnika',
-    isSelected: true
-  }, 
+    isSelected: true,
+  },
   {
-    title: 'Wszystkie', 
-    isSelected: false
-  }, 
+    title: 'Wszystkie',
+    isSelected: false,
+  },
   {
     title: 'Tylko wideo',
-    isSelected: false
-  }, 
+    isSelected: false,
+  },
 ]
 
 const PlayerTestPage = () => (
@@ -31,9 +30,13 @@ const PlayerTestPage = () => (
         <p key={tab}>{tab}</p>
       ))}
     </Tabs>
-    <Button intent="primary">Primary</Button>
-    <Avatar size='small' image={USER_DATA.image} playerName={`${USER_DATA.name} ${USER_DATA.surname}`} />
-    <PlayMakerScore scoreValue={USER_DATA.score} />
+    <Button disabled intent="secondary" text="Siema" size="medium" />
+    <Avatar
+      size="small"
+      image={USER_DATA.image}
+      alt={`${USER_DATA.name} ${USER_DATA.surname}`}
+    />
+    <PlayMakerScore intent="trendUp" scoreValue={USER_DATA.score} />
     <Chips chips={chips} />
   </main>
 )
