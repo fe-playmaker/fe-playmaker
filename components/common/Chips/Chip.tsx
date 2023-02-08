e39c3df7fe7d849605d25dbeb337f6eb6ef24265
Mateusz <michalikkontakt@gmail.com>
2023-02-08 23:00:33 +0100

    removed unneccesary url managment

--- components/common/Chips/Chip.tsx
+++ components/common/Chips/Chip.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { cva, VariantProps } from 'class-variance-authority'
-import Link from 'next/link'
 import type { ReactNode } from 'react'
 
 const chipsCva = cva(['border-2 border-transparent rounded-full'], {
@@ -18,17 +17,10 @@ const chipsCva = cva(['border-2 border-transparent rounded-full'], {
 
 interface ChipsProps extends VariantProps<typeof chipsCva> {
   readonly children: ReactNode
-  readonly query: string
 }
 
-export const Chip = ({ children, size, query, intent }: ChipsProps) => (
-  <Link
-    href={{
-      pathname: '/players',
-      query: { matches: query.toLowerCase() },
-    }}
-    className={chipsCva({ size, intent })}
-  >
+export const Chip = ({ children, size, intent }: ChipsProps) => (
+  <button type="button" className={chipsCva({ size, intent })}>
     {children}
-  </Link>
+  </button>
 )
