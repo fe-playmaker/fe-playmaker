6a515f9cd06034f03668de17ecc85040a29a7398
Mateusz <michalikkontakt@gmail.com>
2023-02-08 23:00:04 +0100

    Added more variants and props to button component

--- components/common/Button/Button.tsx
+++ components/common/Button/Button.tsx
@@ -1,41 +1,36 @@
 import { cva, VariantProps } from 'class-variance-authority'
 import type { ButtonHTMLAttributes, ReactNode } from 'react'
 
-const button = cva(
-  [
-    'font-bold',
-    'border',
-    'rounded-full',
-    'text-white',
-    'border-transparent',
-    'p-2 px-4',
-  ],
+const buttonCva = cva(
+  ['border', 'rounded-full', 'border-transparent', 'transition-all'],
   {
     variants: {
       intent: {
-        primary: ['bg-primary'],
-        secondary: ['bg-secondary'],
+        primary:
+          'bg-dark text-white font-bold hover:bg-primary disabled:bg-light disabled:text-darkShade-10 disabled:hover:bg-light disabled:cursor-default',
+        secondary:
+          'bg-none border border-dark font-medium hover:border-2 disabled:text-darkShade-10 disabled:hover:border disabled:border-darkAlpha-5 disabled:cursor-default',
+        text: 'border-none text-darkAlpha-50 font-medium px-0 py-0 hover:text-tertiary',
       },
       size: {
-        small: ['text-label-md'],
-        big: ['text-label-xl'],
+        large: 'text-label-xl px-9 py-5',
+        medium: 'text-label-lg px-8 py-4',
+        small: 'text-label-md px-6 py-3',
+        extraSmall: 'text-label-sm px-4 py-3',
       },
-    },
-    defaultVariants: {
-      intent: 'primary',
-      size: 'small',
     },
   },
 )
 
 interface Props
-  extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'disabled'>,
-    VariantProps<typeof button> {
-  readonly children: ReactNode
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonCva> {
+  text?: string
+  icon?: ReactNode
 }
 
-export const Button = ({ children, intent, ...props }: Props) => (
-  <button className={button({ intent })} {...props} type="button">
-    {children}
+export const Button = ({ text, intent, size, ...props }: Props) => (
+  <button className={buttonCva({ intent, size })} type="button" {...props}>
+    {text}
   </button>
 )
