4cdc1979b35d0f5f2d1a39246a4e3303300add97
Mateusz <michalikkontakt@gmail.com>
2023-02-10 18:31:36 +0100

    added all of types for button

--- components/common/Button/Button.tsx
+++ components/common/Button/Button.tsx
@@ -1,42 +1,79 @@
 import { cva, VariantProps } from 'class-variance-authority'
-import type { ButtonHTMLAttributes, ReactNode } from 'react'
+import type { ButtonHTMLAttributes, ReactElement } from 'react'
+import { cloneElement } from 'react'
 
 const buttonCva = cva(
   [
-    'border',
-    'rounded-full',
-    'border-transparent',
+    'rounded-full group',
     'transition-all',
     'flex items-center gap-2',
+    'tracking-wide font-bold',
   ],
   {
     variants: {
-      intent: {
-        primary:
-          'bg-dark text-white font-bold hover:bg-primary disabled:bg-light disabled:text-darkShade-10 disabled:hover:bg-light disabled:cursor-default',
-        secondary:
-          'bg-none border border-dark font-medium hover:border-2 disabled:text-darkShade-10 disabled:hover:border disabled:border-darkAlpha-5 disabled:cursor-default',
-        text: 'border-none text-darkAlpha-50 font-medium px-0 py-0 hover:text-tertiary',
+      buttonType: {
+        primary: [
+          'bg-dark text-white hover:bg-primary',
+          'disabled:bg-light disabled:text-darkAlpha-20 disabled:hover:bg-light disabled:cursor-default',
+        ],
+        secondary: [
+          'bg-none outline outline-1 outline-darkAlpha-30',
+          'hover:outline-2 hover:outline-darkShade-50',
+          'disabled:text-darkAlpha-20 disabled:hover:outline-1 disabled:outline-darkAlpha-5 disabled:cursor-default',
+        ],
       },
-      size: {
-        large: 'text-label-xl px-9 py-5',
-        medium: 'text-label-lg px-8 py-4',
-        small: 'text-label-md px-6 py-3',
-        extraSmall: 'text-label-sm px-4 py-3',
+      buttonSize: {
+        large: 'text-label-lg px-9 py-5',
+        medium: 'text-label-md px-8 py-4',
+        small: 'text-label-sm px-6 py-3',
+        extraSmall: 'text-label-xs px-4 py-3',
       },
     },
   },
 )
 
+const iconCva = cva('group-disabled:fill-darkAlpha-20', {
+  variants: {
+    iconType: {
+      primary: 'fill-white',
+      secondary: 'fill-dark',
+    },
+    iconSize: {
+      large: 'w-7 h-7',
+      small: 'w-5 h-5',
+    },
+  },
+})
+
+type ButtonProps = VariantProps<typeof buttonCva>
+type IconProps = VariantProps<typeof iconCva>
+
 interface Props
   extends ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonCva> {
+    ButtonProps,
+    IconProps {
   text?: string
-  icon?: ReactNode
+  icon?: ReactElement
 }
 
-export const Button = ({ text, icon, intent, size, ...props }: Props) => (
-  <button className={buttonCva({ intent, size })} type="button" {...props}>
-    {icon} {text}
+export const Button = ({
+  text,
+  icon,
+  buttonType,
+  buttonSize,
+  iconType,
+  iconSize,
+  ...props
+}: Props) => (
+  <button
+    className={buttonCva({ buttonSize, buttonType })}
+    type="button"
+    {...props}
+  >
+    {!!icon &&
+      cloneElement(icon, {
+        className: iconCva({ iconSize, iconType }),
+      })}
+    {text}
   </button>
 )
