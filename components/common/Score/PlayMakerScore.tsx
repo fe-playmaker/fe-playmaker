b38553e6471470f5c8b5f40dc8b846e9bd92b8ef
Mateusz <michalikkontakt@gmail.com>
2023-02-10 18:33:28 +0100

    added more variants and switching between selected variants

--- components/common/Score/PlayMakerScore.tsx
+++ components/common/Score/PlayMakerScore.tsx
@@ -2,38 +2,71 @@ import { cva, VariantProps } from 'class-variance-authority'
 import TrendDownIcon from 'img/icons/TrendDown.svg'
 import TrendUpIcon from 'img/icons/Trendup.svg'
 
-const pmScoreCva = cva(['text-white', 'p-2'], {
-  variants: {
-    intent: {
-      trendUp: ['bg-greenShade-10'],
-      trendDown: ['bg-orangeShade-10'],
-    },
-    size: {
-      small: ['text-label-md'],
-      big: ['text-label-xl'],
+const scoreCva = cva(
+  [
+    'flex items-center justify-center bg-primaryShade-10 px-3 py-2 font-bold text-primary',
+  ],
+  {
+    variants: {
+      scoreType: {
+        default: ['w-[3.5rem] h-[3rem] text-heading-sm'],
+        hidden: ['w-[5rem] h-[3rem] text-heading-md'],
+      },
     },
   },
-  defaultVariants: {
-    intent: 'trendUp',
-    size: 'small',
-  },
-})
+)
 
-interface Props extends VariantProps<typeof pmScoreCva> {
+const trendCva = cva(
+  [
+    'text-white',
+    'py-0 px-3',
+    'w-[2.5rem] h-[2.5rem]',
+    'flex items-center justify-center',
+  ],
+  {
+    variants: {
+      trendScoreType: {
+        trendUp: ['bg-greenShade-10'],
+        trendDown: ['bg-orangeShade-10'],
+      },
+    },
+  },
+)
+
+type ScoreProps = VariantProps<typeof scoreCva>
+type TrendScoreProps = VariantProps<typeof trendCva>
+
+interface Props extends TrendScoreProps, ScoreProps {
   scoreValue: number
 }
 
-export const PlayMakerScore = ({ intent, scoreValue }: Props) => (
+export const PlayMakerScore = ({
+  trendScoreType,
+  scoreValue,
+  scoreType,
+}: Props) => (
   <div className="flex flex-col items-center gap-2">
-    <div className="flex items-start">
-      <div className="flex h-8 w-8 items-center justify-center bg-primaryShade-10">
-        <span className="font-bold text-primary">{scoreValue}</span>
+    <div
+      className={`flex ${
+        trendScoreType === 'trendUp' ? 'items-start' : 'items-end'
+      }`}
+    >
+      <div className={scoreCva({ scoreType })}>
+        {scoreType !== 'hidden' ? <span>{scoreValue}</span> : <span>?</span>}
       </div>
-      <div className={pmScoreCva({ intent })}>
-        <p>{intent === 'trendUp' ? <TrendUpIcon /> : <TrendDownIcon />}</p>
-      </div>
+      {scoreType !== 'hidden' && (
+        <div className={trendCva({ trendScoreType })}>
+          <p>
+            {trendScoreType === 'trendUp' ? (
+              <TrendUpIcon className="h-6 w-6" />
+            ) : (
+              <TrendDownIcon className="h-6 w-6" />
+            )}
+          </p>
+        </div>
+      )}
     </div>
-    <p className="text-center text-caption-sm">
+    <p className="text-center text-caption-md text-darkAlpha-40">
       Playmaker <br /> score
     </p>
   </div>
