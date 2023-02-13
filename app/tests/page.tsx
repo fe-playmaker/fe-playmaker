16b3d441ddc7ba8c9b62ff637f965fb529789c35
dejwi <misti.krasula@gmail.com>
2023-02-13 22:30:37 +0100

    add banner

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -4,6 +4,7 @@ import SendIcon from 'img/icons/Send.svg'
 import React, { useState } from 'react'
 
 import { Avatar } from '@/components/common/Avatar/Avatar'
+import Banner from '@/components/common/Banner/banner'
 import { Button } from '@/components/common/Button/Button'
 import { Chips } from '@/components/common/Chips/Chips'
 import MatchResult from '@/components/common/Match-result/match-result'
@@ -20,7 +21,7 @@ const TestingPage = () => {
   return (
     <>
       <Navbar />
-      <main>
+      <main className="h-screen bg-light">
         <Tabs tabs={tabs} size="medium">
           {tabs.map(tab => (
             <p key={tab}>{tab}</p>
@@ -58,6 +59,23 @@ const TestingPage = () => {
         <Chips
           chips={['Tylko z udziałem zawodnika', 'Wszystkie', 'Tylko wideo']}
         />
+        <div className="px-5">
+          <Banner
+            btnText="Napisz"
+            btnIcon={<SendIcon />}
+            btnSize="extraSmall"
+            btnIntent="primary"
+            accentClass="before:bg-tertiary"
+          >
+            <span className="text-body-md font-bold">Szukam klubu</span>
+            <span className="text-body-sm font-medium text-darkAlpha-40">
+              Oczekiwania: zwrot za dojazdy...
+            </span>
+            <span className="text-body-sm font-medium text-darkAlpha-40 underline">
+              Więcej
+            </span>
+          </Banner>
+        </div>
       </main>
     </>
   )
---
+++ components/common/Banner/banner.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import clsx from 'clsx'
+
+import { Button } from '../Button/Button'
+
+type ButtonProps = React.ComponentProps<typeof Button>
+
+interface IProps {
+  btnText?: string
+  btnIcon?: ButtonProps['icon']
+  btnIntent?: ButtonProps['intent']
+  btnSize?: ButtonProps['size']
+  children: React.ReactElement | React.ReactElement[]
+  accentClass?: string
+  bgColor?: string
+  onBtnClick?: () => void
+}
+
+const Banner = ({
+  children,
+  accentClass,
+  bgColor,
+  btnIcon,
+  btnSize,
+  btnText,
+  btnIntent,
+  onBtnClick,
+}: IProps) => (
+  <div
+    className={clsx(
+      'flex items-center justify-between px-6 py-5',
+      bgColor || 'bg-white',
+      accentClass &&
+        `relative before:absolute before:left-0 before:h-full before:w-[0.188rem] ${accentClass}`,
+    )}
+  >
+    <div className="flex flex-col  font-inter">{children}</div>
+    <div>
+      {(btnText || btnIcon) && (
+        <Button
+          intent={btnIntent}
+          size={btnSize}
+          icon={btnIcon}
+          text={btnText}
+          onClick={onBtnClick}
+        />
+      )}
+    </div>
+  </div>
+)
+
+export default Banner
