fb9dd36e94470f7506e77201dc684bf104918c4f
Mateusz <michalikkontakt@gmail.com>
2023-02-14 23:38:43 +0100

    added classname prop to section container

--- components/common/SectionContainer/SectionContainer.tsx
+++ components/common/SectionContainer/SectionContainer.tsx
@@ -1,9 +1,13 @@
+import clsx from 'clsx'
 import { ReactNode } from 'react'
 
 interface Props {
   children: ReactNode
+  className?: string
 }
 
-export const SectionContainer = ({ children }: Props) => (
-  <section className="bg-white p-6">{children}</section>
+export const SectionContainer = ({ children, className }: Props) => (
+  <section className={clsx('bg-white px-6 py-8', className)}>
+    {children}
+  </section>
 )
