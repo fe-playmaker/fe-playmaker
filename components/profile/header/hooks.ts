681366594ef1828f93f212c4df36f2888387501f
Mateusz <michalikkontakt@gmail.com>
2023-02-14 23:39:47 +0100

    updated react query hook

--- components/profile/header/hooks.ts
+++ components/profile/header/hooks.ts
@@ -2,8 +2,12 @@ import { useQuery } from '@tanstack/react-query'
 import { returnAfterTime } from 'dummy-api/helpers'
 import { profileHeaderData } from 'dummy-api/profile/header'
 
-export const useProfileHeader = (id: string) =>
-  useQuery({
+export const useProfileHeader = (id: string) => {
+  const { isLoading, isError, isFetching, data } = useQuery({
     queryKey: ['profile-header', id],
     queryFn: returnAfterTime(profileHeaderData, 2000),
+    retry: 3,
   })
+
+  return { isLoading, isError, isFetching, data }
+}
