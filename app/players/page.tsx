a9ac56275b104d8a91104ed1c5160f283fa05342
dejwi <misti.krasula@gmail.com>
2023-02-14 11:50:58 +0100

    add profile header module

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,12 +1,32 @@
 'use client'
 
+import Tabs from '@/components/common/Tabs/tabs'
 import Navbar from '@/components/Navbar/navbar'
+import ProfileHeader from '@/components/profile/header/header'
+
+export const tabs = ['Przegląd', 'Kariera', 'Mecze', 'PlayMaker Score']
 
 const PlayerTestPage = () => (
-  <>
+  <main className="min-h-screen bg-light">
     <Navbar />
-    <main>Player page mockup</main>
-  </>
+    <ProfileHeader />
+    <GradientedBg />
+    <Tabs tabs={tabs} size="medium">
+      {tabs.map(tab => (
+        <p key={tab}>{tab}</p>
+      ))}
+    </Tabs>
+  </main>
 )
 
 export default PlayerTestPage
+
+const GradientedBg = () => (
+  <div
+    className="absolute top-0 h-[30vh] w-screen"
+    style={{
+      background:
+        'linear-gradient(200.96deg, rgba(208, 170, 70, 0.2) 0.66%, rgba(252, 214, 115, 0) 86.72%);',
+    }}
+  />
+)
