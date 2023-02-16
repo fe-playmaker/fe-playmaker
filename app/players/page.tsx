1c99191c1deb7ad73d81751fd5f8f9d037e557e1
dejwi <misti.krasula@gmail.com>
2023-02-16 13:51:44 +0100

    add matches tab

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -4,6 +4,7 @@ import Tabs from '@/components/common/Tabs/tabs'
 import Navbar from '@/components/Navbar/navbar'
 import ProfileCareer from '@/components/profile/career/career'
 import ProfileHeader from '@/components/profile/header/header'
+import ProfileMatches from '@/components/profile/matches/matches'
 
 export const tabs = ['Przegląd', 'Kariera', 'Mecze', 'PlayMaker Score']
 
@@ -15,7 +16,7 @@ const PlayerTestPage = () => (
     <Tabs tabs={tabs} size="medium">
       <span>Przegląd</span>
       <ProfileCareer />
-      <span>Mecze</span>
+      <ProfileMatches />
       <span>PlayMaker Score</span>
     </Tabs>
   </main>
