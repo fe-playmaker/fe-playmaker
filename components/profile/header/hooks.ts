aa942906cab15cca0d5cf20af1d49ed55c8c1361
Mateusz <michalikkontakt@gmail.com>
2023-02-17 01:18:05 +0100

    removed unnecessary destructuring

--- components/profile/header/hooks.ts
+++ components/profile/header/hooks.ts
@@ -2,12 +2,8 @@ import { useQuery } from '@tanstack/react-query'
 import { returnAfterTime } from 'dummy-api/helpers'
 import { profileHeaderData } from 'dummy-api/profile/header'
 
-export const useProfileHeader = (id: string) => {
-  const { isLoading, isError, isFetching, data } = useQuery({
+export const useProfileHeader = (id: string) =>
+  useQuery({
     queryKey: ['profile-header', id],
     queryFn: returnAfterTime(profileHeaderData, 2000),
-    retry: 3,
   })
-
-  return { isLoading, isError, isFetching, data }
-}
