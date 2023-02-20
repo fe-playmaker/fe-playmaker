6c923a386a7d407ae104eb6d002e8d6375252763
dejwi <misti.krasula@gmail.com>
2023-02-20 22:42:29 +0100

    move helper

--- dummy-api/helpers.ts
+++
@@ -1,8 +0,0 @@
-export const returnAfterTime =
-  <T>(data: T, miliseconds: number) =>
-  () =>
-    new Promise<T>(resolve => {
-      setTimeout(() => {
-        resolve(data)
-      }, miliseconds)
-    })
