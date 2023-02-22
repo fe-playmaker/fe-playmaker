f9d090c2ad3a3f2d997ef06ef090b1eb5ab6351d
Mateusz <michalikkontakt@gmail.com>
2023-02-22 22:28:37 +0100

    added information about missing profile video

--- components/profile/overview/gameVideo/gameVideo.tsx
+++ components/profile/overview/gameVideo/gameVideo.tsx
@@ -1,12 +1,31 @@
+import { IProfileOverviewData } from 'dummy-api/profile/overview'
 import GameVideoImage from 'img/illustrations/gameVideo.png'
+import NothingVideoIcon from 'img/illustrations/NothingVideoBox.svg'
 import Image from 'next/image'
 
-export const GameVideo = () => (
-  <Image
-    src={GameVideoImage}
-    width={335}
-    height={188}
-    alt="player video of game"
-    className="w-full"
-  />
-)
+export const GameVideo = ({
+  playerData,
+}: Pick<IProfileOverviewData, 'playerData'>) => {
+  const hasVideo = !!playerData.video
+
+  return (
+    <div>
+      {hasVideo ? (
+        <Image
+          src={GameVideoImage}
+          width={335}
+          height={188}
+          alt="player video of game"
+          className="w-full"
+        />
+      ) : (
+        <div className="flex flex-col items-center gap-5">
+          <NothingVideoIcon className="h-[3.5rem] w-[4.5rem]" />
+          <p className="text-center font-inter text-body-sm text-darkAlpha-20">
+            Zawodnik nie dodał jeszcze żadnego <br /> wideo ze swojej gry
+          </p>
+        </div>
+      )}
+    </div>
+  )
+}
