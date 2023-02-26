36d4d17f95b4adcdde25b592fbbb815a527177a7
dejwi <misti.krasula@gmail.com>
2023-02-26 14:06:14 +0100

    fix gradient bg

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import { TProfileHeader, TProfileOverview } from 'types/profile'
 
 import Tabs from '@/components/common/Tabs/tabs'
@@ -25,7 +25,7 @@ const PlayerTestPage = () => {
   const isLoading = headerLoading || overviewLoading
 
   return (
-    <main className="flex min-h-screen flex-col bg-light">
+    <main className="z-0 flex min-h-screen flex-col bg-light">
       <Navbar />
       {headerData?.premium && <GradientedBg />}
       <AnimatePresence mode="wait">
@@ -57,11 +57,13 @@ const PlayerTestPage = () => {
 export default PlayerTestPage
 
 const GradientedBg = () => (
-  <div
-    className="absolute top-0 right-0 h-[30vh] w-screen"
+  <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1, transition: { delay: 0.1 } }}
+    className="absolute top-0 right-0 z-10 h-[30vh] w-screen"
     style={{
       background:
-        'linear-gradient(200.96deg, rgba(208, 170, 70, 0.2) 0.66%, rgba(252, 214, 115, 0) 86.72%);',
+        'linear-gradient(200.96deg, rgba(208, 170, 70, 0.2) 0.66%, rgba(252, 214, 115, 0) 86.72%)',
     }}
   />
 )
