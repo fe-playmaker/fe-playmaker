54c5d1ebcd0082a957920b1968d823c37aefc586
Mateusz Michalik <michalikkontakt@gmail.com>
2023-03-02 22:20:36 +0100

    removed padding

--- components/profile/overview/inShort/slider.tsx
+++ components/profile/overview/inShort/slider.tsx
@@ -11,7 +11,7 @@ import { TProfileOverview } from 'types/profile'
 import { sliderSettings } from '../helpers'
 
 const SliderItem = ({ title, icon }: TProfileOverview['inShort'][number]) => (
-  <div className="mx-auto flex w-[7rem] flex-col items-center gap-3 p-3 pb-5">
+  <div className="mx-auto flex w-[7rem] flex-col items-center gap-3 p-3 pb-5 ">
     <Image src={icon} alt="icon" width={32} height={32} />
 
     <p className="text-center font-inter text-body-sm font-medium text-darkAlpha-40">
@@ -23,7 +23,7 @@ const SliderItem = ({ title, icon }: TProfileOverview['inShort'][number]) => (
 export const PlayerSlider = ({
   inShort,
 }: Pick<TProfileOverview, 'inShort'>) => (
-  <div className="pb-6">
+  <div className="pb-6 md:pb-0">
     <Slider {...sliderSettings} className="slider">
       {inShort?.map(item => (
         <SliderItem key={item.title} {...item} />
