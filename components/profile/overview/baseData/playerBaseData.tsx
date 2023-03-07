ab12c89a5357935942f212df84e14997a416201f
Mateusz Michalik <michalikkontakt@gmail.com>
2023-03-07 01:14:24 +0100

    Added multiple positions placed in grid

--- components/profile/overview/baseData/playerBaseData.tsx
+++ components/profile/overview/baseData/playerBaseData.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { cva } from 'class-variance-authority'
+import clsx from 'clsx'
 import SoccerField from 'img/illustrations/SoccerField.svg'
 import { TProfileOverview } from 'types/profile'
 
@@ -28,7 +29,7 @@ export const PlayerBaseData = ({
   const baseData = [
     {
       title: 'Wiek',
-      content: `${age} lat (1992)`,
+      content: `${age} lat (${new Date().getFullYear() - age})`,
     },
     {
       title: 'Wzrost',
@@ -61,6 +62,50 @@ export const PlayerBaseData = ({
     },
   ]
 
+  const getPositions = () => {
+    const positions = [
+      { title: 'Napastnik', grid: 'col-start-2 items-center justify-center' },
+      { title: 'Lewy pomocnik', grid: 'row-start-2 items-end justify-end' },
+      {
+        title: 'Prawy pomocnik',
+        grid: 'row-start-2 col-start-3 items-end justify-start',
+      },
+      {
+        title: 'Środkowy pomocnik ofensywny',
+        grid: 'col-start-2 row-start-2 items-center justify-center',
+      },
+      {
+        title: 'Środkowy pomocnik',
+        grid: 'col-start-2 row-start-3 items-start justify-center',
+      },
+      {
+        title: 'Środkowy pomocnik defensywny',
+        grid: 'col-start-2 row-start-3 md:items-end items-center justify-center',
+      },
+      {
+        title: 'Lewy obrońca',
+        grid: 'row-start-4 md:items-end items-center justify-center pl-5',
+      },
+      {
+        title: 'Środkowy obrońca',
+        grid: 'row-start-4 col-start-2 items-center justify-center',
+      },
+      {
+        title: 'Prawy obrońca',
+        grid: 'row-start-4 col-start-3 md:items-end items-center justify-center pr-5',
+      },
+      {
+        title: 'Bramkarz',
+        grid: 'row-start-5 col-start-2 items-center justify-center',
+      },
+    ]
+
+    const mainPosition = positions.find(pos => pos.title === position)
+    const alternatePosition = positions.find(pos => pos.title === altPosition)
+
+    return { mainPosition, alternatePosition }
+  }
+
   return (
     <div>
       <div className="grid grid-cols-1 gap-2 border border-transparent border-b-light pb-7 md:grid-cols-2 md:pb-8">
@@ -84,26 +129,43 @@ export const PlayerBaseData = ({
               className="flex flex-col font-inter text-body-sm font-medium md:flex-row md:items-start md:text-body-md"
             >
               <span className="text-darkAlpha-20 md:w-[7.5rem]">{d.title}</span>
-              <span className="flex items-center gap-2 text-darkAlpha-40 md:gap-3">
-                {d.content}
+              <p className="flex items-center gap-2 text-darkAlpha-40 md:gap-3">
+                <span className="max-w-[9rem]">{d.content}</span>
                 {d.isVisibleDot && (
                   <div
                     className={singleDotCva({
-                      // this is a temporary solution
-                      type: d.content === 'Napastnik' ? 'striker' : 'helper',
+                      type:
+                        d.content === getPositions().mainPosition?.title
+                          ? 'striker'
+                          : 'helper',
                     })}
                   />
                 )}
-              </span>
+              </p>
             </div>
           ))}
         </div>
-        <div className="absolute -right-6 top-0 flex h-full w-[11.719rem] items-center justify-center bg-blueShade-10 pt-8 pb-7 md:-right-8 md:h-[13.75rem] md:w-[23.625rem]">
+        <div className="absolute -right-6 top-0 flex h-full w-[11.719rem] items-center justify-center bg-blueShade-10 pt-8 pb-7 md:-right-8 md:w-[23.625rem]">
           <div className="relative">
             <SoccerField className="h-[8.125rem] w-[10rem] md:h-[11.25rem] md:w-[13.875rem]" />
-            {/* this is temporary solution */}
-            <StrikerIcon className="absolute top-0 right-[4rem] icon-32 md:top-[1rem] md:right-[5.9rem]" />
-            <HelperIcon className="absolute top-[2rem] right-[1rem] icon-32 md:top-[3.2rem] md:right-[2.5rem]" />
+            <div className="absolute inset-0 grid grid-cols-3 grid-rows-6 gap-2">
+              <div
+                className={clsx(
+                  'flex h-9 w-full',
+                  getPositions().mainPosition?.grid,
+                )}
+              >
+                <StrikerIcon className="z-10 flex h-full w-full icon-32 md:h-8 md:w-8" />
+              </div>
+              <div
+                className={clsx(
+                  'flex h-9 w-full',
+                  getPositions().alternatePosition?.grid,
+                )}
+              >
+                <HelperIcon className="flex h-full w-full icon-32 md:h-8 md:w-8" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
