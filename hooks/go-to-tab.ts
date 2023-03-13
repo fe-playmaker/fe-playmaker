cb891718ad06c1b5166fccec5d4c4495c18679f5
Mateusz Michalik <michalikkontakt@gmail.com>
2023-03-13 13:54:12 +0100

    added hook to useGoToTab

--- hooks/go-to-tab.ts
+++ hooks/go-to-tab.ts
@@ -11,7 +11,7 @@ export const useGoToTab = () => {
   const goToTab = (idx: number, tabName: string) => {
     let lastScroll = 999999999
 
-    pushQueryParams(tabName)
+    pushQueryParams({ tab: tabName })
 
     window.scroll({
       top: 0,
