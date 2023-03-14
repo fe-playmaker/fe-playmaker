b3dc8bf975a6ebc918f530a70a334ca321ed3271
Mateusz Michalik <michalikkontakt@gmail.com>
2023-03-14 17:23:47 +0100

    corrected pm score

--- dummy-api/profile/pm-score.ts
+++ dummy-api/profile/pm-score.ts
@@ -1,7 +1,7 @@
 import { TProfilePMScore } from 'types/profile'
 
 export const profilePMScoreData: TProfilePMScore = {
-  comparisonPercentage: 32,
+  comparisonPercentage: 55,
   events: [
     {
       trend: 'up',
